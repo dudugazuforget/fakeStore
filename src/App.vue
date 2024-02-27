eef9d10bcf7fe282379f921e74f48544bb5e1860
dudugazuforget <dudu99744@gmail.com>
2024-02-27 15:12:41 -0300

    testes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
 import ListagemProdutos from '@/components/ListagemProdutos.vue';
+import MenuSuperior from '@/components/MenuSuperior.vue';
 </script>
 
 <template>
+  <MenuSuperior />
   <ListagemProdutos />
 </template>
-
-<style scoped>
-
-</style>
